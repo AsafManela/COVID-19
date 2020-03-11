cc90d0a5ae21ba4e4ac8b1085fdcf32d8632aded
Asaf Manela <AsafManela@users.noreply.github.com>
2020-03-11 13:01:18 -0500

    cleaned up us states timeseries aggregation mess

--- src/sandbox.jl
+++ src/sandbox.jl
@@ -1,10 +1,41 @@
 using CSV, DataFrames, Dates, VegaLite
 
-# using Plots, StatsPlots
 const datadir = joinpath(@__DIR__, "csse_covid_19_data", "csse_covid_19_time_series")
 const figdir = joinpath(@__DIR__, "figures")
 
 dateconv(s) = Date(string(s), "m/d/y") + Dates.Year(2000)
+
+stateisocsv = CSV.File(joinpath(@__DIR__, "geoinfo", "states.csv"))
+stateisomap = Dict(row.state => row.name for row in stateisocsv)
+function cleanstate(s, c) 
+    if c != "US"
+        s, false
+    else
+        if occursin(',', s)
+            if endswith(s, ", D.C.")
+                iso = "DC"
+            else
+                iso = rstrip(s)[end-1:end]
+            end
+            
+            if haskey(stateisomap, iso)
+                stateisomap[iso], false
+            else
+                @warn "iso not found for $s"
+                missing, false
+            end
+        else
+            s, true
+        end
+    end
+end
+
+# # tests
+# cleanstate("Hubei", "Mainland China")
+# cleanstate("bla, NY", "US")
+# cleanstate("New York", "US")
+# cleanstate("Washington, D.C.", "US")
+# cleanstate("Jackson County, OR ", "US")
 
 function ts(status::Symbol)
     df = CSV.File(joinpath(datadir, "time_series_19-covid-$status.csv")) |> DataFrame
@@ -18,14 +49,48 @@ function ts()
     deaths = ts(:Deaths)
     recovered = ts(:Recovered)
 
-    join(join(confirmed, deaths, on=[Symbol("Province/State"), Symbol("Country/Region"), :Lat, :Long, :Date]),
+    cdr = join(join(confirmed, deaths, on=[Symbol("Province/State"), Symbol("Country/Region"), :Lat, :Long, :Date]),
             recovered, on=[Symbol("Province/State"), Symbol("Country/Region"), :Lat, :Long, :Date])
+
+    stateisagg = cleanstate.(cdr[!,Symbol("Province/State")], cdr[!,Symbol("Country/Region")])
+    cdr.State = first.(stateisagg)
+    cdr.Total = last.(stateisagg)
+
+    # totals per state were not calculated until 2020-03-10
+
+    # disaggregated data before change
+    ix = [!ismissing(x) && (x=="US") for x in cdr[!, Symbol("Country/Region")] ] .& (.! cdr.Total)
+    usdisaggpre = cdr[ix,:]
+
+    # manually calculated totals after change are missing other countries
+    ix = [!ismissing(x) && (x=="US") for x in cdr[!, Symbol("Country/Region")] ] .& (cdr.Date .> Date(2020,03,9)) .& (.! cdr.Total)
+    usstates2 = by(cdr[ix,:], [:Date, :State],
+        Confirmed_Dis = :Confirmed => sum,
+        Deaths_Dis = :Deaths => sum,
+        Recovered_Dis = :Recovered => sum
+    ) 
+
+    # state totals from source
+    ix = [!ismissing(x) && (x=="US") for x in cdr[!, Symbol("Country/Region")] ] .& (cdr.Date .> Date(2020,03,9)) .& cdr.Total
+    usstates3 = cdr[ix, [:Date, Symbol("Country/Region"), :Lat, :Long, :State, :Confirmed, :Deaths, :Recovered]]
+
+    # create other countries totals as residual
+    usother = join(usstates2, usstates3, on=[:Date, :State])
+    usother.Confirmed = Int.(clamp.(usother.Confirmed .- usother.Confirmed_Dis, 0, Inf))
+    usother.Deaths = Int.(clamp.(usother.Deaths .- usother.Deaths_Dis, 0, Inf))
+    usother.Recovered = Int.(clamp.(usother.Recovered .- usother.Recovered_Dis, 0, Inf))
+    usother.Total = falses(length(usother.Recovered))
+    usother[!,Symbol("Province/State")] .= "Other"
+
+    usdisagg = vcat(usdisaggpre, usother[:, names(usdisaggpre)])
+
+    # remove totals from cdr and add others
+    cdr = vcat(cdr[(.! cdr.Total), :], usother[:, names(usdisaggpre)])
+
+    cdr
 end
 
 cdr = ts()
-
-# ix = [!ismissing(x) && x == "St. Louis County, MO" for x in cdr[!, Symbol("Province/State")] ];
-# cdr[ix, :]
 
 function aggcdr(cdr; byvar=Symbol("Country/Region"), byvalues=["US"])
     ix = [!ismissing(x) && (x in byvalues) for x in cdr[!, byvar] ];
@@ -37,26 +102,16 @@ function aggcdr(cdr; byvar=Symbol("Country/Region"), byvalues=["US"])
     ) 
 end
 
-plotcountries = ["Mainland China", "Japan", "South Korea", "Italy", "Iran", "Germany", "US", "UK", "Israel"][2:end]
-agg = aggcdr(cdr; byvalues=plotcountries)
+# unique(cdr[Symbol("Country/Region")])
+# plotcountries = ["Mainland China", "Italy", "Japan", "Republic of Korea", "Germany", "US", "UK", "Israel"]
+plotcountries = ["Italy", "Japan", "South Korea", "Germany", "US", "UK", "Israel"]
+plotcountries = ["US", "Israel"]
 
-p = agg |> @vlplot(:line, x=:Date, y=:Confirmed, color=Symbol("Country/Region"), width=800, height=600,
-    config={
-        background="#333",
-        title={color="#fff"},
-        style={"guide-label"={fill="#fff"}, "guide-title"={fill="#fff"}},
-        axis={domainColor="#fff", gridColor="#888", tickColor="#fff"}
-    }
-)
-save(joinpath(figdir, "agg.select.countries.pdf"), p)
+function plotcountrystatus(cdr, countries, status::Symbol=:Confirmed)
+    agg = aggcdr(cdr; byvalues=plotcountries)
 
-agg = aggcdr(cdr; byvalues=["US"])
-
-aggstacked(country) = stack(aggcdr(cdr; byvalues=[country]), Not([:Date, Symbol("Country/Region")]), variable_name=:Status, value_name=:People)
-aggstacked("US")
-p = aggstacked("US") |> 
-    @vlplot(:line, x=:Date, y=:People, color=:Status,
-        width=800, height=600,
+    agg |> @vlplot(x=:Date, y=status, color=Symbol("Country/Region"), width=800, height=600,
+        mark={:line, point={filled=false}},
         config={
             background="#333",
             title={color="#fff"},
@@ -64,6 +119,27 @@ p = aggstacked("US") |>
             axis={domainColor="#fff", gridColor="#888", tickColor="#fff"}
         }
     )
+end
 
+p = plotcountrystatus(cdr, plotcountries, :Confirmed)
 
-# @df agg plot(x=:Date, y=:Confirmed, color=Symbol("Country/Region"))
+save(joinpath(figdir, "agg.select.countries.pdf"), p)
+
+aggstacked(cdr, country) = stack(aggcdr(cdr; byvalues=[country]), Not([:Date, Symbol("Country/Region")]), variable_name=:Status, value_name=:People)
+
+function plotcountry(cdr, country)
+    aggstacked(cdr, country) |> 
+        @vlplot(x=:Date, y=:People, color=:Status, title=country,
+            mark={:line, point={filled=false}},
+            width=800, height=600,
+            config={
+                background="#333",
+                title={color="#fff"},
+                style={"guide-label"={fill="#fff"}, "guide-title"={fill="#fff"}},
+                axis={domainColor="#fff", gridColor="#888", tickColor="#fff"}
+            }
+        )
+end
+
+plotcountry(cdr, "India")
+plotcountry(cdr, "Israel")
